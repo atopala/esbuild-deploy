af4e62342aee5fd323244cc743f95eabf068f0a0
Adrian Topala <a.topala@reply.de>
2025-05-28 10:01:40 +0200

    fixing CI: using Docker

--- packages/testing-e2e/cucumber.mjs
+++ packages/testing-e2e/cucumber.mjs
@@ -2,11 +2,22 @@ export default function () {
    const common = {
       format: [
          "progress-bar",
+         "summary",
          "html:test-results/cucumber-report.html",
-         "json:test-results/cucumber-report.json",
-         // "message:test-results/cucumber.ndjson",
-         "junit:test-results/cucumber-results.xml",
+         // "json:test-results/cucumber-report.json",
+         "message:test-results/cucumber-messages.json",
+         "junit:test-results/cucumber-junit.xml",
       ],
+      formatOptions: {
+         junit: {
+            suiteName: "Cucumber Tests",
+            uniqueTestNames: true,
+         },
+         html: {
+            theme: "bootstrap",
+            reportTitle: "E2E Test Report",
+         },
+      },
       paths: ["./features/*.feature"],
       loader: ["ts-node/esm"],
       import: ["./src/**/*.ts"],
